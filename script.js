fa384482b62c1b0ab601717bd29acd2f97b2dce1
Sebadev15 <seba_el15@live.com>
2023-09-11 16:18:14 -0300

    Cambios finales en JS

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function convertir() {
     let valore = parseInt(document.getElementById("valor").value);
     let resultado = 0;
-    let dolar = 700;
+    let dolar = 710;
     let euro = 770;
 
     if (document.getElementById("uno").checked) {
-        resultado = valore * dolar;
+        resultado = valore / dolar;
         alert("El cambio de Pesos Argentinos a Dólares Estadounidenses es: $" + resultado);
     } else if (document.getElementById("dos").checked) {
-        resultado = valore * euro;
+        resultado = valore / euro;
         alert("El cambio de Pesos Argentinos a Euros es: €" + resultado);
     }
     else {
